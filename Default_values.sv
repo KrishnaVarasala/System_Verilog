01d0a72f68c09551ae311c010ef762ccfd3e55e2
KrishnaVarasala <krishnatejavarasala@gmail.com>
2025-07-30 22:10:59 +0530

    Update and rename Default Values to Default_values.sv

---
+++ Default_values.sv
@@ -0,0 +1,21 @@
+//List out the dat types in system verilog and print the default values
+module tb();
+reg a;
+int b;
+bit c;
+byte d;
+logic e;
+integer f;
+
+initial begin
+  $display("reg : %0d \nint : %0d\nbit : %0d\nbyte : %0d\nlogic :%0d\ninteger : %0d",a,b,c,d,e,f);
+end
+endmodule
+
+output:
+reg : x 
+int : 0
+bit : 0
+byte : 0
+logic :x
+integer : x
