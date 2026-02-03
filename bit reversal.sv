94d8858d86172a313f3d195eb5aba9ee4556349d
KRISHNA VARASALA <krishnatejavarasala@gmail.com>
2026-02-03 20:21:19 +0530

    Update bit reversal.sv

--- bit reversal.sv
+++ bit reversal.sv
@@ -14,3 +14,23 @@ endmodule
 
 //output:
 00001111
+
+////////////////////////////////////////////////////////////////////
+//string reversal
+module tb();
+ 
+  string data = "krishna";
+  int len = data.len();
+  string reverse_data;
+ 
+  initial begin 
+   for(int i = len-1;i>=0;i--)begin
+    reverse_data = {reverse_data,data[i]};
+   end
+    $display("%0s",reverse_data);
+  end
+endmodule
+
+output:
+anhsirk
+
