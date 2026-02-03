7ab192fe5eec63abf5f91a2e3b438d2c34f2cdfc
KRISHNA VARASALA <krishnatejavarasala@gmail.com>
2026-02-03 20:27:30 +0530

    Update bit reversal.sv

--- bit reversal.sv
+++ bit reversal.sv
@@ -1,12 +1,10 @@
 //Bit reversal
-// Code your testbench here
-// or browse Examples
 module tb();
-  bit[7:0] a = 8'b11110000;
-  bit [7:0] b;
+  bit[7:0] a = 8'b11110000;      //input value
+  bit [7:0] b;                   //variable to store output
   
   initial begin
-    b = {<<{a}};
+    b = {<<{a}};                 //using operator to reverse the bit
     $display("%b",b);
   end
 endmodule
@@ -19,15 +17,15 @@ endmodule
 //string reversal
 module tb();
  
-  string data = "krishna";
-  int len = data.len();
-  string reverse_data;
+  string data = "krishna";                        //input string 
+  int len = data.len();                           //variable to calculate the length --> later we will use in for loop
+  string reverse_data;                            //variable to store the reversed string data
  
   initial begin 
-   for(int i = len-1;i>=0;i--)begin
-    reverse_data = {reverse_data,data[i]};
+    for(int i = len-1;i>=0;i--)begin             //iterates from the end, stops if i reaches the 0th index
+      reverse_data = {reverse_data,data[i]};     //stores the every element in the index along with previous element
    end
-    $display("%0s",reverse_data);
+    $display("%0s",reverse_data);               //displaying the reverese odered string
   end
 endmodule
 
