2076e07cc882d352c448b465c1d76e27b8d49064
KRISHNA VARASALA <krishnatejavarasala@gmail.com>
2026-03-05 23:24:41 +0530

    Create coverage_que.sv

---
+++ Interview_quetions/coverage_que.sv
@@ -0,0 +1,5 @@
+Q.) Why coverage is important although Assertions are there?
+A.) In case of vacuous pass assertion will pass but never that condition will get execute
+  ✔ Assertions pass
+ ❌ But verification is useless
+This is why coverage is important.
